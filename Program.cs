efa24dc9c6c66c360d7a8720ef4b7c15b35722b3
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-13 23:58:01 +0300

    Розв'язання

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n =");
+            int count = int.Parse(Console.ReadLine());
+            double result = 0;
+            for (int a = 1; a <= count; a++)
+            {
+                result = result = 1 / a;
+            }
+            Console.WriteLine($"r={result}"); 
+        }
+    }
+}
